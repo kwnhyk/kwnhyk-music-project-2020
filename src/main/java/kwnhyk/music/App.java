4b358dc801c6e7a4af2e294ca3edbe33ba3260e9
kwnhyk <yunhuk93@gmail.com>
2019-12-18 16:35:13 +0900

    2222출력 삭제

--- src/main/java/kwnhyk/music/App.java
+++ src/main/java/kwnhyk/music/App.java
@@ -9,7 +9,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello Music World");
         System.out.println("1111");
-        System.out.println("2222");
 
     }
 }
