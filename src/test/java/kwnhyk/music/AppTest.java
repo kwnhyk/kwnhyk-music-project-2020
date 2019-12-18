628be6d94e8466fb9dfe7cbc691da796beb3e311
kwnhyk <kwnhyk@naver.com>
2019-12-18 16:24:41 +0900

    1111출력

--- src/test/java/kwnhyk/music/AppTest.java
+++ src/test/java/kwnhyk/music/AppTest.java
@@ -5,10 +5,11 @@ package kwnhyk.music;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-
+/*
 public class AppTest {
     @Test public void testAppHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
     }
 }
+*/
