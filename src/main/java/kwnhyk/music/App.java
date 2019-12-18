106aecb15fcdaf7cbc11ce141d26908cfd5ad6f7
kwnhyk <yunhuk93@gmail.com>
2019-12-18 16:03:57 +0900

    프로젝트 초기화

---
+++ src/main/java/kwnhyk/music/App.java
@@ -0,0 +1,12 @@
+//프로젝트: 음악 정보 사이
+package kwnhyk.music;
+
+public class App {
+ //   public String getGreeting() {
+    //    return "Hello world.";
+ //   }
+
+    public static void main(String[] args) {
+        System.out.println("Hello Music World");
+    }
+}
