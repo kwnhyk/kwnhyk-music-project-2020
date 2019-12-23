ad0efcd7a76139046b4b12bdb1baebdf2e382383
kwnhyk <yunhuk93@gmail.com>
2019-12-23 18:49:21 +0900

    class

--- src/main/java/kwnhyk/music/App2.java
+++ src/main/java/kwnhyk/music/App2.java
@@ -6,40 +6,40 @@ import java.util.Scanner;
 public class App2 { 
   public static void main(String[] args) {
     Scanner keyboard = new Scanner(System.in);
+    class ArtistInfo {
+        int no;
+        String artist;
+        String realName;
+        Date bornDate;
+      }
 
     final int SIZE = 100;
-
-    int[] no = new int[SIZE];
-    String[] artist = new String[SIZE];
-   
-    
-    
-    String[] realName = new String[SIZE];
-    Date[] bornDate = new Date[SIZE];
-
+    ArtistInfo [] infos = new ArtistInfo[SIZE];
+  
     int count = 0;
     
     for (int i = 0; i < SIZE; i++) {
       count++;
+      ArtistInfo info = new ArtistInfo();
       
       System.out.print("번호? ");
-      no[i] = keyboard.nextInt();
+      info.no = keyboard.nextInt();
       keyboard.nextLine(); // 줄바꿈 기호 제거용
 
       System.out.print("아티스트명? ");
-      artist[i] = keyboard.nextLine();
+      info.artist = keyboard.nextLine();
 
       System.out.print("본명? ");
-      realName[i] = keyboard.nextLine();
+      info.realName = keyboard.nextLine();
 
 
 
       System.out.print("출생일? ");
       // "yyyy-MM-dd" 형태로 입력된 문자열을 날짜 정보로 바꾼다.
-      bornDate[i] = Date.valueOf(keyboard.next());
-
-      keyboard.nextLine(); // 출시일 입력 값 다음에 남아 있는 줄바꿈 값 제거
+      info.bornDate= Date.valueOf(keyboard.next());
       
+      keyboard.nextLine(); // 출시일 입력 값 다음에 남아 있는 줄바꿈 값 제거
+      infos[i] = info;
       System.out.print("계속 입력하시겠습니까?(Y/n) ");
       String response = keyboard.nextLine();
       if (!response.equalsIgnoreCase("y"))
@@ -50,8 +50,9 @@ public class App2 {
     System.out.println();
 
     for (int i = 0; i < count; i++) {
+    	ArtistInfo info = infos[i];
       System.out.printf("%d, %s, %s, %s\n", 
-          no[i], artist[i], realName[i], bornDate[i]);
+          info.no, info.artist, info.realName, info.bornDate);
     }
   }
 }
