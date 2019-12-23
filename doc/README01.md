c7fe52b386c02f90727b68eeb8602eb6ec3aa0df
kwnhyk <yunhuk93@gmail.com>
2019-12-23 17:59:09 +0900

    array input

---
+++ doc/README01.md
@@ -0,0 +1,80 @@
+01 - 배열과 흐름 제어문 활용하여 음악 정보 관리
+
+ 목표
+•배열을 활용하여 여러 개의 데이터를 저장할 수 있다.
+•조건문과 반복문을 활용하여 프로그램의 실행 흐름을 제어할 수 있다.
+
+소스 및 결과
+•src/main/java/kwnhyk/music/App 추가
+•src/main/java/kwnhyk/music/App2.java 추가
+
+
+실습
+
+작업1) 여러 개의 음악 정보를 입력 받아 출력하라.
+
+App.java 실행 결과
+
+목록으로 출력할 항목은 번호,음악제목,아티스트명,작곡가명,장르,출시일 이다.
+번호? 1
+음악제목? like me better
+아티스트명? Lauv
+작곡가명? Lauv
+장르? pop
+출시일? 2017
+
+
+계속 입력하시겠습니까?(Y/n) y
+
+번호? 1
+음악제목? like me better
+아티스트명? Lauv
+작곡가명? Lauv
+장르? pop
+출시일? 2017
+
+계속 입력하시겠습니까?(Y/n) y
+
+번호? 1
+음악제목? like me better
+아티스트명? Lauv
+작곡가명? Lauv
+장르? pop
+출시일? 2017
+
+계속 입력하시겠습니까?(Y/n) n
+
+1, like me better     , Lauv , Lauv ,pop ,2017
+2, like me better     , Lauv , Lauv ,pop ,2017
+3, like me better     , Lauv , Lauv ,pop ,2017
+
+작업2) 여러 명의 아티스트 정보를 입력 받아 출력하라.
+
+App2.java 실행 결과
+
+목록으로 출력할 항목은 번호,아티스트명,출생일,본명 이다.
+번호? 1
+출생일? 1994-08-08
+아티스트명? Lauv
+본명? Ari Staprans Leff
+
+
+계속 입력하시겠습니까?(Y/n) y
+
+번호? 2
+출생일? 1994-08-08
+아티스트명? Lauve
+본명? Ari Staprans Leff
+
+계속 입력하시겠습니까?(Y/n) y
+
+번호? 3
+출생일? 1994-08-08
+아티스트명? Lauvr
+본명? Ari Staprans Leff
+
+계속 입력하시겠습니까?(Y/n) n
+
+1, Lauv , Ari Staprans Leff , 1994-08-08 
+2, Lauve , Ari Staprans Leff , 1994-08-08 
+3, Lauvr , Ari Staprans Leff , 1994-08-08  
--- src/main/java/kwnhyk/music/App.java
+++ src/main/java/kwnhyk/music/App.java
@@ -1,13 +1,73 @@
-//프로젝트: 음악 정보 사이
+//프로젝트: 음악 정보 사이트
 package kwnhyk.music;
 
+import java.sql.Date;
+import java.util.Scanner;
+
 public class App {
- //   public String getGreeting() {
-    //    return "Hello world.";
- //   }
 
-    public static void main(String[] args) {
-        System.out.println("Hello Music World");
+  public static void main(String[] args) {
 
+    Scanner keyboard = new Scanner(System.in);
+
+    final int SIZE = 100;
+
+    int[] no = new int[SIZE];
+    String[] title = new String[SIZE];
+    String[] artist = new String[SIZE];
+    int[] startDate = new int[SIZE];
+    String[] writer = new String[SIZE];
+    String[] genre = new String[SIZE];
+     
+   
+
+    int count = 0;
+    
+    for (int i = 0; i < SIZE; i++) {
+      count++;
+      
+      System.out.print("번호? ");
+      no[i] = keyboard.nextInt();
+
+      keyboard.nextLine(); // nextInt() 후에 남아 있는 줄바꿈 기호를 제거한다.
+
+      System.out.print("음악제목? ");
+      title[i] = keyboard.nextLine(); 
+
+      System.out.print("아티스트명? ");
+      artist[i] = keyboard.nextLine();
+
+      System.out.print("작곡가명? ");
+      writer[i] = keyboard.nextLine();
+
+      System.out.print("장르? ");
+      genre[i] = keyboard.nextLine();
+
+      System.out.print("출시일? ");
+     
+      startDate[i] = keyboard.nextInt();
+      
+      keyboard.nextLine();
+
+      
+      
+      System.out.print("계속 입력하시겠습니까?(Y/n) ");
+      String response = keyboard.nextLine();
+      if (!response.equalsIgnoreCase("y"))
+        break;
     }
+    System.out.println();
+
+    for (int i = 0; i < count; i++) {
+      System.out.printf("%d, %s, %s, %s,%s,%d\n",
+          no[i], title[i],artist[i],writer[i],genre[i] ,startDate[i]);
+    }
+
+    keyboard.close();
+  }
 }
+
+
+
+
+
