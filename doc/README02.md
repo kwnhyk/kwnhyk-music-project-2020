be9bf52d49d1a07262523b4d7788c18221c85249
kwnhyk <kwnhyk@naver.com>
2019-12-23 18:49:21 +0900

    class
      Author:    kwnhyk <kwnhyk@naver.com>

---
+++ doc/README02.md
@@ -0,0 +1,34 @@
+# 02 - 클래스로 데이터 타입 정의하기
+
+##  목표
+
+- 클래스를 활용하여 여러 개의 데이터를 한 단위로 묶을 수 있다.
+- 클래스와 인스턴스의 관계를 이해한다.
+- 인스턴스와 레퍼런스의 관계를 이해한다.
+- 레퍼런스 배열을 이해한다.
+
+## 실습 소스 및 결과
+
+- •src/main/java/kwnhyk/music/MusicInfo 추가
+- src/main/java/kwnhyk/music/ArtistInfo.java 추가
+
+- src/main/java/kwnhyk/music/App.java 변경
+- src/main/java/kwnhyk/music/App2.java 변경
+
+
+## 실습
+
+### 작업1) `음악` 데이터를 저장할 데이터 타입을 정의하라.
+
+-MusicInfo.java
+    - 음악 데이터를 저장할 새 데이터 타입을 정의한다.
+- App.java
+    -MusicInfo 클래스를 사용하여 수업 데이터를 다룬다.
+
+### 작업2) `아티스트` 데이터를 저장할 데이터 타입을 정의하라.
+
+- ArtistInfo.java
+    - 아티스트 데이터를 저장할 새 데이터 타입을 정의한다.
+- App2.java
+    - ArtistInfo 클래스를 사용하여 회원 데이터를 다룬다.
+
--- src/main/java/kwnhyk/music/App.java
+++ src/main/java/kwnhyk/music/App.java
@@ -9,65 +9,74 @@ public class App {
   public static void main(String[] args) {
 
     Scanner keyboard = new Scanner(System.in);
-
+    class MusicInfo {
+        int no;
+        String title;
+        String artist;
+    
+        String writer;
+        String genre;
+        int startDate;
+        
+      }
     final int SIZE = 100;
-
-    int[] no = new int[SIZE];
-    String[] title = new String[SIZE];
-    String[] artist = new String[SIZE];
-    int[] startDate = new int[SIZE];
-    String[] writer = new String[SIZE];
-    String[] genre = new String[SIZE];
+  // Lesson 인스턴스 주소를 담을 레퍼런스 배열을 만든다.
+    MusicInfo[] musics = new MusicInfo[SIZE];
+   
      
    
 
     int count = 0;
-    
     for (int i = 0; i < SIZE; i++) {
-      count++;
-      
-      System.out.print("번호? ");
-      no[i] = keyboard.nextInt();
-
-      keyboard.nextLine(); // nextInt() 후에 남아 있는 줄바꿈 기호를 제거한다.
-
-      System.out.print("음악제목? ");
-      title[i] = keyboard.nextLine(); 
-
-      System.out.print("아티스트명? ");
-      artist[i] = keyboard.nextLine();
-
-      System.out.print("작곡가명? ");
-      writer[i] = keyboard.nextLine();
-
-      System.out.print("장르? ");
-      genre[i] = keyboard.nextLine();
-
-      System.out.print("출시일? ");
+        count++;
+  
+        MusicInfo music = new MusicInfo();
+        
+        System.out.print("번호? ");
+        music.no = keyboard.nextInt();
+  
+        keyboard.nextLine(); // nextInt() 후에 남아 있는 줄바꿈 기호를 제거한다.
+  
+        System.out.print("음악제목? ");
+        music.title = keyboard.nextLine();
+  
+        System.out.print("아티스트명? ");
+        music.artist = keyboard.nextLine();
+        System.out.print("작곡가? ");
+        music.writer = keyboard.nextLine();
+        System.out.print("장르? ");
+        music.genre = keyboard.nextLine();
+  
+        System.out.print("출시일? ");
      
-      startDate[i] = keyboard.nextInt();
-      
-      keyboard.nextLine();
-
-      
-      
-      System.out.print("계속 입력하시겠습니까?(Y/n) ");
-      String response = keyboard.nextLine();
-      if (!response.equalsIgnoreCase("y"))
-        break;
+        music.startDate = keyboard.nextInt();
+  
+        
+        keyboard.nextLine(); //  출시일 입력 값 다음에 남아 있는 줄바꿈 값 제거
+        
+        // 정보를 담고 있는 인스턴스의 주소를 나중에 사용할 수 있도록 
+        // 레퍼런스 배열에 보관해 둔다.
+        musics[i] = music;
+        
+        System.out.print("계속 입력하시겠습니까?(Y/n) ");
+        String response = keyboard.nextLine();
+        if (!response.equalsIgnoreCase("y"))
+          break;
+      }
+      System.out.println();
+  
+      for (int i = 0; i < count; i++) {
+       MusicInfo music = musics[i];
+        System.out.printf("%d, %s, %s , %s,%s,%d\n",
+            music.no, music.title, 
+            music.artist, music.genre,music.writer,
+            music.startDate
+            );
+      }
+  
+      keyboard.close();
     }
-    System.out.println();
-
-    for (int i = 0; i < count; i++) {
-      System.out.printf("%d, %s, %s, %s,%s,%d\n",
-          no[i], title[i],artist[i],writer[i],genre[i] ,startDate[i]);
-    }
-
-    keyboard.close();
   }
-}
-
-
-
-
-
+  
+  
+  
