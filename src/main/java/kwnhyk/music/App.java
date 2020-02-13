ba1274c4fff7cef1e8c870e2f0fefc6e32e306e5
kwnhyk <kwnhyk@naver.com>
2020-02-13 17:44:40 +0900

    Exception

--- src/main/java/kwnhyk/music/App.java
+++ src/main/java/kwnhyk/music/App.java
@@ -89,8 +89,14 @@ public class App {
 			
 			Command commandHandler = commandMap.get(command);
 			if(commandHandler!=null) {
+				try {
 				commandHandler.excute();
-			}	else {
+			}	catch(Exception e){
+				System.out.printf("명령어 실행 중 오류 발생:%s\n",e.getMessage());
+			}
+				
+			}
+				else {
 			
 			
 					System.out.println("실행할 수 없는 명령입니다.");
